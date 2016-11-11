16990426678293be744048da48fea474952b4c8d
TIYZAP <ironyardzap@gmail.com>
2016-11-11 17:30:39 -0500

    Tag Fix

--- main.js
+++ main.js
@@ -14,29 +14,29 @@ $.getJSON('https://damp-depths-50705.herokuapp.com/api/notes/')
 })
 
 
-  $('#note').on('submit', function(ev){
-    ev.preventDefault()
-    // toggle_loading('#note')
-    $.post('https://damp-depths-50705.herokuapp.com/api/notes/',
-    $(this).serialize())
-  })
+$('#note').on('submit', function(ev){
+  ev.preventDefault()
+  $.post('https://damp-depths-50705.herokuapp.com/api/notes/',
+  $(this).serialize())
+})
 
-  $('#notes').on('click', '.tag', function(ev){
-    ev.preventDefault()
-    $('#notes').html('')
-    $.getJSON('https://damp-depths-50705.herokuapp.com/api/notes/tag/' +
-    $(this).html())
-    .then(function(r){
-      $('#header').append(':' + r.tag.name)
-      r.tag.notes.forEach(function(note){
-        var display = note_template(note)
-        $('#notes').prepend(display)
-        console.log(tag)
-      })
+$('#notes').on('click', '.tag', function(ev){
+  ev.preventDefault()
+  $('#notes').html('')
+  $.getJSON('https://damp-depths-50705.herokuapp.com/api/notes/tag/' +
+  encodeURIComponent($(this).html()))
+  .then(function(r){
+    $('#header').html('')
+    $('#header').append('Notemeister 5000:' + r.tag.name)
+    r.tag.notes.forEach(function(note){
+      var display = note_template(note)
+      $('#notes').prepend(display)
+      console.log(tag)
     })
   })
+})
 
-
-
-// ).done(function(response){
-//   form_handler('#note')
+// if(window.location.hash.match(/#\d+/).length > 0) {
+//   id = window.location.hash.substring(1)
+//   $('#photo_holder').modal('show')
+// }
